f3a0a86e67ae184674a4f74c4d02ab7411f70436
Angel <iangeleb19@gmail.com>
2023-02-02 13:15:08 +0100

    Commit 5

--- Cancion.java
+++ Cancion.java
@@ -3,4 +3,14 @@ public class Cancion
     private String nombre;
     private String autor;
     private PlayList playlist;
+
+    /**
+     * Constructor for objects of class PlayList
+     */
+    public Cancion(String nombre, String autor, PlayList playlist)
+    {
+        this.autor = autor;
+        this.nombre = nombre;
+        this.playlist = playlist;
+    }
 }

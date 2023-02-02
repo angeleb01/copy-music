dd02484c5e0ac16021c3a0e78cd889f470c47b03
Angel <iangeleb19@gmail.com>
2023-02-02 12:36:33 +0100

    Commit 1

---
+++ PlayList.java
@@ -0,0 +1,14 @@
+
+public class PlayList
+{
+    private int idPlayList;
+    private String nombre;
+    /**
+     * Constructor for objects of class PlayList
+     */
+    public PlayList(int idPlayList, String nombre)
+    {
+        this.idPlayList = idPlayList;
+        this.nombre = nombre;
+    }
+}
